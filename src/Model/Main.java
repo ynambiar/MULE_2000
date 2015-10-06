a489d2efed1100668a73f69a8a8d547b5709a349
ashika789 <ashika789@gmail.com>
2015-10-06 18:49:20 -0400

    Adding in music
    music stuff is added/commented out for now until I can get it to access
    the file not through my computer path

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,9 +1,29 @@
 package Model;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
 import Controller.MasterController;
+import com.sun.media.jfxmedia.track.AudioTrack;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import sun.audio.AudioStream;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioData;
 
+import java.applet.AudioClip;
+import java.io.FileInputStream;
+import sun.audio.ContinuousAudioDataStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import java.net.URL;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javafx.application.Platform;
+import sun.misc.IOUtils;
+import java.awt.event.WindowEvent;
+import java.io.File;
 /**
  * Created by tuckerlocicero on 10/2/15.
  */
@@ -20,7 +40,29 @@ public class Main extends Application {
         stage.setTitle("M.U.L.E. - Deep Thought");
         stage.setResizable(false);
         stage.show();
+//        try {
+//            startMusic();
+//        } catch (Exception e) {
+//            System.out.println("Error with music!");
+//        }
     }
+//    public void startMusic() throws Exception {
+//        try {
+//            Path path = Paths.get("/Users/AshikaGanesh/Documents/Final_Github/MULE_2000/src/View/Resources/music.wav");
+    //  NEED TO FIND A WAY TO ACCESS THAT MUSIC FILE!!
+    // also need a way to break and stop music if system quits.
+//            String soundFile = path.toString();
+//
+//            InputStream in = new FileInputStream(soundFile);
+//            AudioStream audioStream = new AudioStream(in);
+//            AudioPlayer.player.start(audioStream);
+//            } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//
+//    }
+
+
 
     public static void main(String[] args) {
         launch(args);
