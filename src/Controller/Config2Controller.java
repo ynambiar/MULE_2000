51fec84ae814110e9ecea2f4426eb218850806fa
stithwilliam <stithwilliam@gmail.com>
2015-10-06 22:56:09 -0400

    Revert Bug fixes

--- src/Controller/Config2Controller.java
+++ src/Controller/Config2Controller.java
@@ -52,25 +52,23 @@ public class Config2Controller {
     private boolean comboBoxesAreVerififed() {
         Game game = Main.myGame;
         boolean verified = false;
-        ArrayList<Player> players = new ArrayList<>();
         if (combo1human.getValue() != "Not Playing" && combo1race.getValue() != null && p1Name.getText() != null) {
-            players.add(new Player(combo1human.getValue(), combo1race.getValue(), p1Name.getText(), "DODGERBLUE"));
+            game.addPlayer(new Player(combo1human.getValue(), combo1race.getValue(), p1Name.getText(), "DODGERBLUE"));
         }
         if (combo2human.getValue() != "Not Playing" && combo2race.getValue() != null && p2Name.getText() != null) {
-            players.add(new Player(combo2human.getValue(), combo2race.getValue(), p2Name.getText(), "#ff321f"));
+            game.addPlayer(new Player(combo2human.getValue(), combo2race.getValue(), p2Name.getText(), "#ff321f"));
         }
         if (combo3human.getValue() != "Not Playing" && combo3race.getValue() != null && p3Name.getText() != null) {
-            players.add(new Player(combo3human.getValue(), combo3race.getValue(), p3Name.getText(), "#dd02cf"));
+            game.addPlayer(new Player(combo3human.getValue(), combo3race.getValue(), p3Name.getText(), "#dd02cf"));
         }
         if (combo4human.getValue() != "Not Playing" && combo4race.getValue() != null && p4Name.getText() != null) {
-            players.add(new Player(combo4human.getValue(), combo4race.getValue(), p4Name.getText(), "#ffa639"));
+            game.addPlayer(new Player(combo4human.getValue(), combo4race.getValue(), p4Name.getText(), "#ffa639"));
         }
-        if (players.size() > 1) {
+        if (game.getPlayers().size() > 1) {
             verified = true;
-            for (Player p : game.getPlayers()) {
-                game.addPlayer(p);
-                System.out.println(p.toString());
-            }
+        }
+        for (Player p : game.getPlayers()) {
+            System.out.println(p.toString());
         }
         return verified;
     }
