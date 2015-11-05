2700e89583b2245eae409497f8c8bbbb4335d0b3
ashika789 <ashika789@gmail.com>
2015-11-05 09:05:41 -0500

    this is taking too long -_-

--- src/Controller/StartTurnController.java
+++ src/Controller/StartTurnController.java
@@ -1,40 +1,47 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import model.Game;
 import model.Main;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
+
 
 import java.io.Serializable;
+/**
+* Start Turn Controller.
+*/
 
 public class StartTurnController implements Serializable {
+  //JAVADOC
+  @FXML
+  private Label startTurnLabel;
+  @FXML
+  private Button startBtn;
 
-    @FXML
-    private Label startTurnLabel;
-    @FXML
-    private Button startBtn;
-
-    /**
-     * Sets labels for the scene.
-     */
-    public void initialize() {
-        Game game = Main.myGame;
-        if (game.getCurrentPlayer() != null) {
-            if (game.getRoundNumber() > 0) {
-                startTurnLabel.setText("Round " + game.getRoundNumber() + ": " + game.getCurrentPlayer().getName() + "'s turn!");
-            } else {
-                startTurnLabel.setText("Land Selection Phase: " + game.getCurrentPlayer().getName() + "'s turn!");
-            }
-        }
+  /**
+  * Sets labels for the scene.
+  */
+  public final void initialize() {
+    Game game = Main.myGame;
+    if (game.getCurrentPlayer() != null) {
+      if (game.getRoundNumber() > 0) {
+        startTurnLabel.setText("Round " + game.getRoundNumber() + ": "
+            + game.getCurrentPlayer().getName() + "'s turn!");
+      } else {
+        startTurnLabel.setText("Land Selection Phase: "
+            + game.getCurrentPlayer().getName() + "'s turn!");
+      }
     }
+  }
 
-    @FXML
-    /**
-     * Sets Start button function.
-     */
-    public void setStartBtn(MouseEvent event) {
-        Main.myGame.startTurn();
-    }
+  @FXML
+  /**
+   * Sets Start button function.
+   * @param event MouseEvent
+   */
+  public final void setStartBtn(final MouseEvent event) {
+    Main.myGame.startTurn();
+  }
 }
