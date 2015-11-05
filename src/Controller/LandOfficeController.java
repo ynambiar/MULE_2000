29f068b7ca13383f4e6abd28e678ee920ba77610
ashika789 <ashika789@gmail.com>
2015-11-05 09:16:54 -0500

    blah

--- src/Controller/LandOfficeController.java
+++ src/Controller/LandOfficeController.java
@@ -1,68 +1,74 @@
 package controller;
 
-import model.Game;
-import model.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import model.Game;
+import model.Main;
 
 import java.io.Serializable;
 import java.util.Random;
 
+/**
+* Land Office Controller class.
+*/
+
 public class LandOfficeController implements Serializable {
+  //JAVADOC
+  @FXML
+  private Button yesBuyLandBtn;
+  @FXML
+  private Button noBuyLandBtn;
+  @FXML
+  private Button yesSellLandBtn;
+  @FXML
+  private Label buyPrice;
+  @FXML
+  private Label sellPrice;
 
-    @FXML
-    private Button yesBuyLandBtn;
-    @FXML
-    private Button noBuyLandBtn;
-    @FXML
-    private Button yesSellLandBtn;
-    @FXML
-    private Label buyPrice;
-    @FXML
-    private Label sellPrice;
+  private int buyingPrice;
+  private int sellingPrice;
 
-    int buyingPrice, sellingPrice;
+  @FXML
+  /**
+  * Set Yes Buy Land button and sets land cost.
+  */
+  private void setYesBuyLandBtn() {
+    Main.myGame.setPhase("Purchasing Land");
+    Main.myGame.setLandCost(buyingPrice);
+    MasterController.getInstance().loadMapScene();
+    Main.myGame.refreshLabels();
+  }
 
-    @FXML
-    /**
-     * Set Yes Buy Land button and sets land cost.
-     */
-    private void setYesBuyLandBtn() {
-        Main.myGame.setPhase("Purchasing Land");
-        Main.myGame.setLandCost(buyingPrice);
-        MasterController.getInstance().loadMapScene();
-        Main.myGame.refreshLabels();
-    }
+  @FXML
+  /**
+  * Sets No button and loads town screen.
+  */
+  private void setNoBuyLandBtn() {
+    MasterController.getInstance().loadTownScene();
+  }
 
-    @FXML
-    /**
-     * Sets No button and loads town screen.
-     */
-    private void setNoBuyLandBtn() {
-        MasterController.getInstance().loadTownScene();
-    }
+  @FXML
+  /**
+  * Set Yes Sell Land button and sets land selling price.
+  */
+  private void setYesSellLandBtn() {
+    Main.myGame.setPhase("Selling Land");
+    Main.myGame.setLandCost(sellingPrice);
+    MasterController.getInstance().loadMapScene();
+    Main.myGame.refreshLabels();
+  }
 
-    @FXML
-    /**
-     * Set Yes Sell Land button and sets land selling price.
-     */
-    private void setYesSellLandBtn() {
-        Main.myGame.setPhase("Selling Land");
-        Main.myGame.setLandCost(sellingPrice);
-        MasterController.getInstance().loadMapScene();
-        Main.myGame.refreshLabels();
-    }
-
-    /**
-     * Initialize Land Office controller.
-     */
-    public void initialize() {
-        Game g = Main.myGame;
-        Random random = new Random(g.getRoundNumber() + g.getCurrentPlayer().hashCode() + g.hashCode());
-        buyingPrice = 300 + Main.myGame.getRoundNumber() * random.nextInt(100);
-        sellingPrice = 400 + random.nextInt(200);
-        this.buyPrice.setText("Buying Price: " + buyingPrice);
-        this.sellPrice.setText("Selling Price: " + sellingPrice);
-    }
+  /**
+  * Initialize Land Office controller.
+  */
+  public final void initialize() {
+    Game gameset = Main.myGame;
+    Random random = new Random(gameset.getRoundNumber()
+        + gameset.getCurrentPlayer().hashCode() + gameset.hashCode());
+    buyingPrice = 300 + Main.myGame.getRoundNumber() * random.nextInt(100);
+    sellingPrice = 400 + random.nextInt(200);
+    this.buyPrice.setText("Buying Price: " + buyingPrice);
+    this.sellPrice.setText("Selling Price: " + sellingPrice);
+  }
 }
--- src/Controller/StartController.java
+++ src/Controller/StartController.java
@@ -1,37 +1,40 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
 
 import java.io.Serializable;
+/**
+* Start Controller Class.
+**/
 
 public class StartController implements Serializable {
+  //JAVADOC
+  @FXML
+  private Button ruleBtn;
+  @FXML
+  private Button playBtn;
+  @FXML
+  private Button loadBtn;
 
-    @FXML
-    private Button ruleBtn;
-    @FXML
-    private Button playBtn;
-    @FXML
-    private Button loadBtn;
+  @FXML
+  private void setRuleBtn() {
+    // MasterController.getInstance().loadAsteroidScene();
+  }
 
-    @FXML
-    private void setRuleBtn() {
-        //MasterController.getInstance().loadAsteroidScene();
-    }
+  @FXML
+  /**
+  * Sets Play button to load Config 1 scene.
+  */
+  private void setPlayBtn() {
+    MasterController.getInstance().loadConfig1Scene();
+  }
 
-    @FXML
-    /**
-     * Sets Play button to load Config 1 scene.
-     */
-    private void setPlayBtn() {
-        MasterController.getInstance().loadConfig1Scene();
-    }
-
-    @FXML
-    /**
-     * Sets Load button to Load screen scene.
-     */
-    private void setLoadBtn() {
-        MasterController.getInstance().loadLoadScreenScene();
-    }
+  @FXML
+  /**
+   * Sets Load button to Load screen scene.
+   */
+  private void setLoadBtn() {
+    MasterController.getInstance().loadLoadScreenScene();
+  }
 }
