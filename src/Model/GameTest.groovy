ea6036f3c3187a8447d9c2055cc0cc6a3c31b4ea
Tucker LoCicero <t.g.locicero@gmail.com>
2015-11-11 00:32:44 -0500

    added my unit

--- src/Model/GameTest.groovy
+++ src/Model/GameTest.groovy
@@ -33,4 +33,18 @@ public class GameTest {
         }
     }
 
+    @Test
+    public void testNumMules() {
+        Player p = new Player();
+        p.setMuleEmplaced(0, 0, Mule.FOOD);
+        assertEquals(1, p.getNumMules());
+        p.setMuleEmplaced(5, 9, Mule.SMITHORE);
+        assertEquals(2, p.getNumMules());
+        p.setMuleEmplaced(5, 0, Mule.FLOWER);
+        assertEquals(3, p.getNumMules());
+        p.setMuleEmplaced(0, 9, Mule.ENERGY);
+        assertEquals(4, p.getNumMules());
+    }
+}
+
 }
