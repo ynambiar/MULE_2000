f0ef77f376c944710927b42d5048e6d0b3233e7f
ashika789 <ashika789@gmail.com>
2015-12-03 22:34:42 -0500

    OMFG GRADLE WORK!!!!
    in command line, cd into the MULE_2000 folder and then run
    “gradle-2.6/bin/gradle run”
    it should work

--- src/main/java/com/mule/Controller/MasterController.java
+++ src/main/java/com/mule/Controller/MasterController.java
@@ -13,313 +13,313 @@ import java.io.Serializable;
  */
 public class MasterController implements Serializable {
 
-  /**
-    Stage.
-   */
-  private Stage stage;
+    /**
+     Stage.
+     */
+    private Stage stage;
 
-  /**
-    Config 1 scene.
-   */
-  private Scene config1Scene;
+    /**
+     Config 1 scene.
+     */
+    private Scene config1Scene;
 
-  /**
-    Config 2 scene.
-   */
-  private Scene config2Scene;
+    /**
+     Config 2 scene.
+     */
+    private Scene config2Scene;
 
-  /**
-    Inside pub scene.
-   */
-  private Scene insidePubScene;
+    /**
+     Inside pub scene.
+     */
+    private Scene insidePubScene;
 
-  /**
-    Inside store scene.
-   */
-  private Scene insideStoreScene;
+    /**
+     Inside store scene.
+     */
+    private Scene insideStoreScene;
 
-  /**
-    Store transaction scene for food.
-   */
-  private Scene storeTransactionSceneFood;
+    /**
+     Store transaction scene for food.
+     */
+    private Scene storeTransactionSceneFood;
 
-  /**
-    Store transaction scene for energy.
-   */
-  private Scene storeTransactionSceneEnergy;
+    /**
+     Store transaction scene for energy.
+     */
+    private Scene storeTransactionSceneEnergy;
 
-  /**
-    Store transaction scene for smithore.
-   */
-  private Scene storeTransactionSceneSmithore;
+    /**
+     Store transaction scene for smithore.
+     */
+    private Scene storeTransactionSceneSmithore;
 
-  /**
-    Inside town scene.
-   */
-  private Scene insideTownScene;
+    /**
+     Inside town scene.
+     */
+    private Scene insideTownScene;
 
-  /**
-    Rules scene.
-   */
-  private Scene rulesScene;
+    /**
+     Rules scene.
+     */
+    private Scene rulesScene;
 
-  /**
-    Map scene.
-   */
-  private Scene mapScene;
+    /**
+     Map scene.
+     */
+    private Scene mapScene;
 
-  /**
-    Start scene.
-   */
-  private Scene startScene;
+    /**
+     Start scene.
+     */
+    private Scene startScene;
 
-  /**
-    Mule transaction scene.
-   */
-  private Scene muleTransactionScene;
+    /**
+     Mule transaction scene.
+     */
+    private Scene muleTransactionScene;
 
-  /**
-    Load game scene.
-   */
-  private Scene loadGameScreen;
+    /**
+     Load game scene.
+     */
+    private Scene loadGameScreen;
 
-  /**
-    Map main.java.com.mule.Controller.
-   */
-  private MapController mapController;
+    /**
+     Map main.java.com.mule.Controller.
+     */
+    private MapController mapController;
 
 
-  /**
-   * Creates a singleton
-   */
-  private final static MasterController masterController
-          = new MasterController();
+    /**
+     * Creates a singleton
+     */
+    private final static MasterController masterController
+            = new MasterController();
 
-  /**
-   * Returns the single instance of MasterController within the entire scope of
-   * the project.
-   *
-   * @return MasterController
-   */
-  public static MasterController getInstance() {
-    return masterController;
-  }
-
-  /**
-   * Constructor for Master main.java.com.mule.Controller. Loads all the scenes.
-   */
-  private MasterController() {
-    Parent root;
-    try {
-      root = FXMLLoader.load(getClass().getResource("/main/resources/View/Config1.fxml"));
-      config1Scene = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource("/main/resources/View/Config2.fxml"));
-      config2Scene = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource("/main/resources/View/Pub.fxml"));
-      insidePubScene = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource("/main/resources/View/Store.fxml"));
-      insideStoreScene = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/StoreTransactionScreenFood.fxml"));
-      storeTransactionSceneFood = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/StoreTransactionScreenEnergy.fxml"));
-      storeTransactionSceneEnergy = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/StoreTransactionScreenSmithore.fxml"));
-      storeTransactionSceneSmithore = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/MuleTransactionScreen.fxml"));
-      muleTransactionScene = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource("/main/resources/View/Town.fxml"));
-      insideTownScene = new Scene(root);
-
-      FXMLLoader loader = new FXMLLoader(getClass()
-              .getResource("/main/resources/View/Map.fxml"));
-      root = loader.load();
-      mapScene = new Scene(root);
-      mapController = loader.getController();
-      System.out.println("master main.java.com.mule.Controller" + mapController);
-
-      root = FXMLLoader
-          .load(getClass().getResource("/main/resources/View/RulesScreen.fxml"));
-      rulesScene = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/loadGameScreen.fxml"));
-      loadGameScreen = new Scene(root);
-
-      root = FXMLLoader.load(getClass().getResource("/main/resources/View/Start.fxml"));
-      startScene = new Scene(root);
-
-    } catch (IOException e) {
-      System.out.println("MasterController(): " + e);
+    /**
+     * Returns the single instance of MasterController within the entire scope of
+     * the project.
+     *
+     * @return MasterController
+     */
+    public static MasterController getInstance() {
+        return masterController;
     }
-  }
 
-  /**
-   * Load Config1 scene.
-   */
-  public final void loadConfig1Scene() {
-    stage.setScene(config1Scene);
-  }
+    /**
+     * Constructor for Master main.java.com.mule.Controller. Loads all the scenes.
+     */
+    private MasterController() {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/View/Config1.fxml"));
+            config1Scene = new Scene(root);
 
-  /**
-   * Load Config 2 scene.
-   */
-  public final void loadConfig2Scene() {
-    stage.setScene(config2Scene);
-  }
+            root = FXMLLoader.load(getClass().getResource("/View/Config2.fxml"));
+            config2Scene = new Scene(root);
 
-  /**
-   * Load Pub scene.
-   */
-  public final void loadPubScene() {
-    stage.setScene(insidePubScene);
-  }
+            root = FXMLLoader.load(getClass().getResource("/View/Pub.fxml"));
+            insidePubScene = new Scene(root);
 
-  /**
-   * Load Store scene.
-   */
-  public final void loadStoreScene() {
-    stage.setScene(insideStoreScene);
-  }
+            root = FXMLLoader.load(getClass().getResource("/View/Store.fxml"));
+            insideStoreScene = new Scene(root);
 
-  /**
-   * Load food Store Transaction Scene.
-   */
-  public final void loadStoreTransactionSceneFood() {
-    stage.setScene(storeTransactionSceneFood);
-  }
+            root = FXMLLoader.load(getClass().getResource(
+                    "/View/StoreTransactionScreenFood.fxml"));
+            storeTransactionSceneFood = new Scene(root);
 
-  /**
-   * Load energy Store Transaction Scene.
-   */
-  public final void loadStoreTransactionSceneEnergy() {
-    stage.setScene(storeTransactionSceneEnergy);
-  }
+            root = FXMLLoader.load(getClass().getResource(
+                    "/View/StoreTransactionScreenEnergy.fxml"));
+            storeTransactionSceneEnergy = new Scene(root);
 
-  /**
-   * Load smithore Store Transaction Scene.
-   */
-  public final void loadStoreTransactionSceneSmithore() {
-    stage.setScene(storeTransactionSceneSmithore);
-  }
+            root = FXMLLoader.load(getClass().getResource(
+                    "/View/StoreTransactionScreenSmithore.fxml"));
+            storeTransactionSceneSmithore = new Scene(root);
 
-  /**
-   * Load mule Transaction Scene.
-   */
-  public final void loadMuleTransactionScene() {
-    stage.setScene(muleTransactionScene);
-  }
+            root = FXMLLoader.load(getClass().getResource(
+                    "/View/MuleTransactionScreen.fxml"));
+            muleTransactionScene = new Scene(root);
 
-  /**
-   * Load town Scene.
-   */
-  public final void loadTownScene() {
-    stage.setScene(insideTownScene);
-  }
+            root = FXMLLoader.load(getClass().getResource("/View/Town.fxml"));
+            insideTownScene = new Scene(root);
 
-  /**
-   * Load Map Scene.
-   */
-  public final void loadMapScene() {
-    stage.setScene(mapScene);
-  }
+            FXMLLoader loader = new FXMLLoader(getClass()
+                    .getResource("/View/Map.fxml"));
+            root = loader.load();
+            mapScene = new Scene(root);
+            mapController = loader.getController();
+            System.out.println("master main.java.com.mule.Controller" + mapController);
 
-  /**
-   * Load Rules Scene.
-   */
-  public final void loadRulesScene() {
-    stage.setScene(rulesScene);
-  }
+            root = FXMLLoader
+                    .load(getClass().getResource("/View/RulesScreen.fxml"));
+            rulesScene = new Scene(root);
 
-  /**
-   * Load Start Scene.
-   */
-  public final void loadStartScene() {
-    stage.setScene(startScene);
-  }
+            root = FXMLLoader.load(getClass().getResource(
+                    "/View/loadGameScreen.fxml"));
+            loadGameScreen = new Scene(root);
 
-  /**
-   * Load Load Screen.
-   */
-  public final void loadLoadScreenScene() {
-    stage.setScene(loadGameScreen);
-  }
+            root = FXMLLoader.load(getClass().getResource("/View/Start.fxml"));
+            startScene = new Scene(root);
 
-  /**
-   * Load Event Scene.
-   */
-  public final void loadEventScene() {
-    try {
-      stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/EventScreen.fxml"))));
-    } catch (IOException e) {
-      System.out.println("IO EXCEPTION");
+        } catch (IOException e) {
+            System.out.println("MasterController(): " + e);
+        }
     }
-  }
 
-  /**
-   * Load Land Office Scene.
-   */
-  public final void loadLandOfficeScene() {
-    try {
-      stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/LandOffice.fxml"))));
-    } catch (IOException e) {
-      System.out.println("IO EXCEPTION");
+    /**
+     * Load Config1 scene.
+     */
+    public final void loadConfig1Scene() {
+        stage.setScene(config1Scene);
     }
-  }
 
-  /**
-   * Load Start Turn Scene.
-   */
-  public final void loadStartTurnScene() {
-    try {
-      stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/StartTurn.fxml"))));
-    } catch (IOException e) {
-      System.out.println("IO EXCEPTION");
+    /**
+     * Load Config 2 scene.
+     */
+    public final void loadConfig2Scene() {
+        stage.setScene(config2Scene);
     }
-  }
 
-  /**
-   * Load Gambling Results Scene.
-   */
-  public final void loadGamblingResultsScene() {
-    try {
-      stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
-              "/main/resources/View/GamblingResults.fxml"))));
-    } catch (IOException e) {
-      System.out.println("IO EXCEPTION");
+    /**
+     * Load Pub scene.
+     */
+    public final void loadPubScene() {
+        stage.setScene(insidePubScene);
     }
-  }
 
-  /**
-   * Return MapController.
-   *
-   * @return MapController
-   */
-  public final MapController getMapController() {
-    return mapController;
-  }
+    /**
+     * Load Store scene.
+     */
+    public final void loadStoreScene() {
+        stage.setScene(insideStoreScene);
+    }
 
-  /**
-   * Sets stage to given Stage.
-   *
-   * @param stage Stage
-   */
-  public final void setStage(final Stage stage) {
-    this.stage = stage;
-  }
+    /**
+     * Load food Store Transaction Scene.
+     */
+    public final void loadStoreTransactionSceneFood() {
+        stage.setScene(storeTransactionSceneFood);
+    }
+
+    /**
+     * Load energy Store Transaction Scene.
+     */
+    public final void loadStoreTransactionSceneEnergy() {
+        stage.setScene(storeTransactionSceneEnergy);
+    }
+
+    /**
+     * Load smithore Store Transaction Scene.
+     */
+    public final void loadStoreTransactionSceneSmithore() {
+        stage.setScene(storeTransactionSceneSmithore);
+    }
+
+    /**
+     * Load mule Transaction Scene.
+     */
+    public final void loadMuleTransactionScene() {
+        stage.setScene(muleTransactionScene);
+    }
+
+    /**
+     * Load town Scene.
+     */
+    public final void loadTownScene() {
+        stage.setScene(insideTownScene);
+    }
+
+    /**
+     * Load Map Scene.
+     */
+    public final void loadMapScene() {
+        stage.setScene(mapScene);
+    }
+
+    /**
+     * Load Rules Scene.
+     */
+    public final void loadRulesScene() {
+        stage.setScene(rulesScene);
+    }
+
+    /**
+     * Load Start Scene.
+     */
+    public final void loadStartScene() {
+        stage.setScene(startScene);
+    }
+
+    /**
+     * Load Load Screen.
+     */
+    public final void loadLoadScreenScene() {
+        stage.setScene(loadGameScreen);
+    }
+
+    /**
+     * Load Event Scene.
+     */
+    public final void loadEventScene() {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
+                    "/View/EventScreen.fxml"))));
+        } catch (IOException e) {
+            System.out.println("IO EXCEPTION");
+        }
+    }
+
+    /**
+     * Load Land Office Scene.
+     */
+    public final void loadLandOfficeScene() {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
+                    "/View/LandOffice.fxml"))));
+        } catch (IOException e) {
+            System.out.println("IO EXCEPTION");
+        }
+    }
+
+    /**
+     * Load Start Turn Scene.
+     */
+    public final void loadStartTurnScene() {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
+                    "/View/StartTurn.fxml"))));
+        } catch (IOException e) {
+            System.out.println("IO EXCEPTION");
+        }
+    }
+
+    /**
+     * Load Gambling Results Scene.
+     */
+    public final void loadGamblingResultsScene() {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
+                    "/View/GamblingResults.fxml"))));
+        } catch (IOException e) {
+            System.out.println("IO EXCEPTION");
+        }
+    }
+
+    /**
+     * Return MapController.
+     *
+     * @return MapController
+     */
+    public final MapController getMapController() {
+        return mapController;
+    }
+
+    /**
+     * Sets stage to given Stage.
+     *
+     * @param stage Stage
+     */
+    public final void setStage(final Stage stage) {
+        this.stage = stage;
+    }
 
 }
