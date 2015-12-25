0f40013592312b81ead083651f149a3693e5bbfe
Tucker LoCicero <t.g.locicero@gmail.com>
2015-12-24 23:09:39 -0700

    Revert "Revert "gradle bugs a bit - can still earn a few points for attempy. player IO problem still there""
    This reverts commit 774e2360900d27c3188b8cdc2edc8177ca10edf6.

--- src/main/java/com/mule/Controller/Config1Controller.java
+++ src/main/java/com/mule/Controller/Config1Controller.java
@@ -9,142 +9,142 @@ import main.java.com.mule.Model.Map.MapType;
 
 import java.io.Serializable;
 /**
-Config1Controller class.
-*/
+ Config1Controller class.
+ */
 public class Config1Controller implements Serializable {
 
-  /**
-   Anchor pane.
-  */
-  @FXML
-  private AnchorPane pane;
+    /**
+     Anchor pane.
+     */
+    @FXML
+    private AnchorPane pane;
 
-  /**
-   Back button.
-  */
-  @FXML
-  private Button backBtn;
+    /**
+     Back button.
+     */
+    @FXML
+    private Button backBtn;
 
-  /**
-   Next button.
-  */
-  @FXML
-  private Button nextBtn;
+    /**
+     Next button.
+     */
+    @FXML
+    private Button nextBtn;
 
-  /**
-   Difficulty toggle.
-  */
-  @FXML
-  private ToggleGroup difficultyToggle;
+    /**
+     Difficulty toggle.
+     */
+    @FXML
+    private ToggleGroup difficultyToggle;
 
-  /**
-   Map toggle.
-  */
-  @FXML
-  private ToggleGroup mapToggle;
+    /**
+     Map toggle.
+     */
+    @FXML
+    private ToggleGroup mapToggle;
 
-  /**
-   Easy radio button.
-  */
-  @FXML
-  private RadioButton easyRadio;
+    /**
+     Easy radio button.
+     */
+    @FXML
+    private RadioButton easyRadio;
 
-  /**
-   Medium radio button.
-  */
-  @FXML
-  private RadioButton mediumRadio;
+    /**
+     Medium radio button.
+     */
+    @FXML
+    private RadioButton mediumRadio;
 
-  /**
-   Hard radio button.
-  */
-  @FXML
-  private RadioButton hardRadio;
+    /**
+     Hard radio button.
+     */
+    @FXML
+    private RadioButton hardRadio;
 
-  /**
-   Standard radio button.
-  */
-  @FXML
-  private RadioButton standardMapRadio;
+    /**
+     Standard radio button.
+     */
+    @FXML
+    private RadioButton standardMapRadio;
 
-  /**
-   Random map radio button.
-  */
-  @FXML
-  private RadioButton randomMapRadio;
+    /**
+     Random map radio button.
+     */
+    @FXML
+    private RadioButton randomMapRadio;
 
-  /**
-   Error Label.
-  */
-  @FXML
-  private Label errorLabel;
+    /**
+     Error Label.
+     */
+    @FXML
+    private Label errorLabel;
 
-  /**
-   Config set boolean.
-  */
-  private boolean configsAreSet = false;
+    /**
+     Config set boolean.
+     */
+    private boolean configsAreSet = false;
 
-  /**
-  Set back button.
-  */
-  @FXML
-  private void setBackBtn() {
-    MasterController.getInstance().loadStartScene();
-  }
-
-  @FXML
-  /**
-  * Sets action for the next button.
-  */
-  private void setNextBtn() {
-    boolean toggles[] = {false, false};
-    main.java.com.mule.Model.Game game = Main.myGame;
-    difficultyToggle.getToggles().stream().filter(Toggle::isSelected).forEach(t -> {
-      toggles[0] = true;
-      if (easyRadio.isSelected()) {
-        game.setDifficulty(Difficulty.EASY);
-      }
-      if (mediumRadio.isSelected()) {
-        game.setDifficulty(Difficulty.MEDIUM);
-      }
-      if (hardRadio.isSelected()) {
-        game.setDifficulty(Difficulty.HARD);
-      }
-      System.out.println(game.getDifficulty());
-    });
-    mapToggle.getToggles().stream().filter(Toggle::isSelected).forEach(t -> {
-      toggles[1] = true;
-      if (standardMapRadio.isSelected()) {
-        game.setMapType(MapType.STANDARD);
-      }
-      if (randomMapRadio.isSelected()) {
-        game.setMapType(MapType.RANDOM);
-      }
-      System.out.println(Main.myGame.getMapType());
-    });
-    if (toggles[0] && toggles[1]) {
-      configsAreSet = true;
+    /**
+     Set back button.
+     */
+    @FXML
+    private void setBackBtn() {
+        MasterController.getInstance().loadStartScene();
     }
-    if (configsAreSet) {
-      MasterController.getInstance().getMapController().createMap(
-          game.getMapType());
-      MasterController.getInstance().loadConfig2Scene();
-      // Driver.mygame.createMap();
-    } else {
-      errorLabel.setVisible(true);
-    }
-  }
 
-  /**
-  * Intilializes toggle groups.
-  */
-  public final void initialize() {
-    mapToggle = new ToggleGroup();
-    difficultyToggle = new ToggleGroup();
-    easyRadio.setToggleGroup(difficultyToggle);
-    mediumRadio.setToggleGroup(difficultyToggle);
-    hardRadio.setToggleGroup(difficultyToggle);
-    standardMapRadio.setToggleGroup(mapToggle);
-    randomMapRadio.setToggleGroup(mapToggle);
-  }
+    @FXML
+    /**
+     * Sets action for the next button.
+     */
+    private void setNextBtn() {
+        boolean toggles[] = {false, false};
+        main.java.com.mule.Model.Game game = Main.myGame;
+        difficultyToggle.getToggles().stream().filter(Toggle::isSelected).forEach(t -> {
+            toggles[0] = true;
+            if (easyRadio.isSelected()) {
+                game.setDifficulty(Difficulty.EASY);
+            }
+            if (mediumRadio.isSelected()) {
+                game.setDifficulty(Difficulty.MEDIUM);
+            }
+            if (hardRadio.isSelected()) {
+                game.setDifficulty(Difficulty.HARD);
+            }
+            System.out.println(game.getDifficulty());
+        });
+        mapToggle.getToggles().stream().filter(Toggle::isSelected).forEach(t -> {
+            toggles[1] = true;
+            if (standardMapRadio.isSelected()) {
+                game.setMapType(MapType.STANDARD);
+            }
+            if (randomMapRadio.isSelected()) {
+                game.setMapType(MapType.RANDOM);
+            }
+            System.out.println(Main.myGame.getMapType());
+        });
+        if (toggles[0] && toggles[1]) {
+            configsAreSet = true;
+        }
+        if (configsAreSet) {
+            MasterController.getInstance().getMapController().createMap(
+                    game.getMapType());
+            MasterController.getInstance().loadConfig2Scene();
+            // Driver.mygame.createMap();
+        } else {
+            errorLabel.setVisible(true);
+        }
+    }
+
+    /**
+     * Intilializes toggle groups.
+     */
+    public final void initialize() {
+        mapToggle = new ToggleGroup();
+        difficultyToggle = new ToggleGroup();
+        easyRadio.setToggleGroup(difficultyToggle);
+        mediumRadio.setToggleGroup(difficultyToggle);
+        hardRadio.setToggleGroup(difficultyToggle);
+        standardMapRadio.setToggleGroup(mapToggle);
+        randomMapRadio.setToggleGroup(mapToggle);
+    }
 }
